9ea127f1a99814f4d14aac4b7efb75403980abfa
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2024-11-21 23:06:53 -0500

    login

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,15 +1,37 @@
 
 import { Link as RouterLink } from 'react-router-dom';
-import { Google } from "@mui/icons-material";
 import { Button, Grid2, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks'
 
+
+const formData = {
+  email: 'prueba@gmail.com',
+  password: '123456',
+  displayName: 'Prueba',
+}
+
+const formValidation = {
+  email: [(value) => value.includes('@'), 'El correo no es valido'],
+  password: [(value) => value.length >= 6, 'La contraseña debe tener al menos 6 caracteres'],
+  displayName: [(value) => value.length >= 3, 'El nombre debe tener al menos 3 caracteres'],
+}
 
 export const RegisterPage = () => {
+  
+  const {displayName, email, password, onInputChange, formState, 
+    isFormValid, displayNameValid, emailValid, passwordValid} = useForm(formData, formValidation);
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+
   return (
-    
+
     <AuthLayout title='Registro - Crear Cuenta'>
-      <form>
+      <form onSubmit={onSubmit}>
               <Grid2 container spacing={2}>
               <Grid2 size={ 12 } sx={{mt:2}}>
                   <TextField
@@ -18,6 +40,9 @@ export const RegisterPage = () => {
                     autoComplete="off"
                     placeholder='Digita tu nombre'
                     fullWidth
+                    name = 'displayName'
+                    value={displayName}
+                    onChange={onInputChange}
                   />
                 </Grid2>
                 <Grid2 size={ 12 } sx={{mt:2}}>
@@ -27,6 +52,9 @@ export const RegisterPage = () => {
                     autoComplete="off"
                     placeholder='correo@gmail.com'
                     fullWidth
+                    name = 'email'
+                    value={email}
+                    onChange={onInputChange}
                   />
                 </Grid2>
                 <Grid2 size={ 12 } sx={{mt:2}}>
@@ -36,11 +64,14 @@ export const RegisterPage = () => {
                     autoComplete="off"
                     placeholder='contraseña'
                     fullWidth
+                    name = 'password'
+                    value={password}
+                    onChange={onInputChange}
                   />
                 </Grid2>
 
                    <Grid2  size={{ xs:12 }} >
-                    <Button variant="contained" fullWidth>
+                    <Button type="submit" variant="contained" fullWidth>
                       Crear Cuenta
                     </Button>
                   </Grid2>
